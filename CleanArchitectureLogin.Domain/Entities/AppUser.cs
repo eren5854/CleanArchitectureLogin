c9aa97b7e9470a19b4d8123eb629b5307dd88474
eren5854 <ihsanerendelibas@hotmail.com>
2024-03-23 04:08:25 +0300

    Domain katmanı eklendi

---
+++ CleanArchitectureLogin.Domain/Entities/AppUser.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CleanArchitectureLogin.Domain.Entities;
+public sealed class AppUser : IdentityUser<Guid>
+{
+    public string FirstName { get; set; } = string.Empty;
+}
